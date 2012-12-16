71971cdf2f7d049407787465ac1738768d1f0969
Danny Joris <danny_joris@yahoo.co.uk>
2012-12-16 15:58:54 -0400

    updated normalize.css + added sticky footer

--- js/script.js
+++ js/script.js
@@ -1,5 +1,28 @@
 (function($) {
   $(document).ready(function() {
-    // script
+    // push footer init
+    pushFooter();
+    $(window).resize(function() {
+      pushFooter();
+    });
+
+    
+    
   });
+  
+  // push footer to the bottom of the page if total content height is smaller
+  // than window height.
+  function pushFooter() {
+    var windowHeight = $(window).innerHeight();
+    var mainHeight = $("#main").height();
+    var headerHeight = $("#header").height();
+    var footerHeight = $("#footer").height();
+
+    if (headerHeight + mainHeight + footerHeight < windowHeight) {
+      $(".footer-wrapper").addClass("sticky-footer");
+    }
+    else {
+      $(".footer-wrapper").removeClass("sticky-footer");
+    }
+  }
 }(jQuery));
