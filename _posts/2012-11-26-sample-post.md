040e102a04f3297bf1fcbcf7ae423fb1b4a2355f
Danny Joris <danny_joris@yahoo.co.uk>
2012-12-17 00:35:50 -0400

    added gist and demo page

--- _posts/2012-11-26-sample-post.md
+++ _posts/2012-11-26-sample-post.md
@@ -1,9 +1,14 @@
 ---
 layout: default
 title: Sample post
+gist: 4248293
 description: lorem ipsum
 summary: lorem ipsum
 category: sample
 tags: abc def
 ---
-Sample post
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vel tortor odio.
+Aliquam posuere sem ac leo fringilla auctor vel et justo. Nulla facilisi. Ut non
+ risus at dolor ullamcorper commodo. Vestibulum eu felis ut lacus condimentum 
+posuere. Donec aliquam, lorem eget dapibus dignissim, mi nisl rutrum orci, in 
+fringilla eros mi non leo.
--- js/script.js
+++ js/script.js
@@ -6,8 +6,39 @@
       pushFooter();
     });
 
+
+//   $("#content").load("https://gist.github.com/raw/4248293/b643eab9ac24175f0183c161d6d438251c6e6b81/index.html pre");
+
+//   $("#content").load("http://api.jquery.com/visual/");
+   //$("#content").load("/about #header");
     
+    var gistId = $("#gist-code").attr('gist');
+    if (gistId) {
+      getGist(gistId);
+    }
+
+
+    // check for hash in url
+    console.log(window.location, "url");
+
+    if (window.location.pathname == "/demo/") {
+      if (window.location.hash) {
+        var gist = window.location.hash.substring(1);
+        console.log(gist);
+        $("body").html('<iframe src="http://bl.ocks.org/d/' + gist + '" id="demo" style="width: 100%; height: 100%; position: absolute; top: 0; right: 0; bottom: 0; left: 0; border: none;"></iframe>');
+//        $("#demo").attr("src", "http://bl.ocks.org/d/" + gist);
+        console.log("http://bl.ocks.org/d/" + gist);
+      }
+      else {
+console.log("nope");
+      }
+    }
     
+
+
+
+
+
   });
   
   // push footer to the bottom of the page if total content height is smaller
@@ -25,4 +56,27 @@
       $(".footer-wrapper").removeClass("sticky-footer");
     }
   }
+  
+  // Gist api callback
+  function getGist(gistId) {
+    $.ajax({
+      url: 'https://api.github.com/gists/' + gistId, 
+      dataType: 'jsonp',
+      success: printGist 
+    });
+  }
+  
+  function printGist(gist) {
+    var files = gist.data.files;
+    var length = files.length,
+    element = null;
+    for (var i = 0; i < length; i++) {
+
+    }
+    
+    
+    console.log(files);
+    //$("#gist-wrapper").html();
+  }
+  
 }(jQuery));
